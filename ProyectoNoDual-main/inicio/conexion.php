7a56e597bfae6b2146d6a581162ac9fd54fb5ab0
irenecg01 <irene.s332806@cesurformacion.com>
2023-02-15 13:52:19 +0100

    base de datos

--- ProyectoNoDual-main/inicio/conexion.php
+++ ProyectoNoDual-main/inicio/conexion.php
@@ -1,12 +1,11 @@
 <?php
 $servername = "localhost";
- $username = "root"; 
- $contraseña = "";
-  $dbname = "mascotas"; 
-  $conn = mysqli_connect($servername, $username, $contraseña, $dbname); 
-  if (!$conn) { 
-    die("Conexión fallida: " . mysqli_connect_error()); 
-} echo "Conexión exitosa";
+$username = "root";
+$contraseña = "";
+$dbname = "ipet";
+$conn = mysqli_connect($servername, $username, $contraseña, $dbname);
+if (!$conn) {
+  die("Conexión fallida: " . mysqli_connect_error());
+}
+//echo "Conexión exitosa";
 ?>
-
-
--- ProyectoNoDual-main/inicio/registrar.php
+++ ProyectoNoDual-main/inicio/registrar.php
@@ -1,13 +1,36 @@
 <?php
-    include("conexion.php");
-    $nombre=$_GET["nombre"];
-    $usuario=$_GET["usuario"];
-    $contraseña=$_GET["contraseña"];
-    $correo=$_GET["correo"];
+    $validate = true;
 
-    $base->query("INSERT INTO usuarios (nombre,usuario,contraseña,correo) VALUES ('$nombre', '$usuario','$contraseña', '$correo');");
-    header("Location:/");
+    if( !isset($_POST["nombre"]) ) $validate = false;
+    if( !isset($_POST["usuario"]) ) $validate = false;
+    if( !isset($_POST["correo"]) ) $validate = false;
+    if( !isset($_POST["contraseña"]) || (strlen($_POST["contraseña"])<2) ) $validate = false;
 
+    if( $validate ){
 
+        $nombre=$_POST["nombre"];
+        $usuario=$_POST["usuario"];
+        $contrasena=$_POST["contraseña"];
+        $correo=$_POST["correo"];
+
+        include("conexion.php");
+
+        $query = "INSERT INTO usuarios (nombre,usuario,contrasena,correo) VALUES ( ?, ? ,? , ?) ";
+
+        $q = $conn->prepare($query);
+        $q->bind_param("ssss", $nombre, $usuario, $contrasena, $correo);
+        $q->execute();
+
+        echo $q->affected_rows;
+
+        if( $q->affected_rows == 1){
+            // Usuario insertado correctamente
+            header("location: /index.php");
+
+        } else {
+
+        }
+
+    }
 
 ?>
