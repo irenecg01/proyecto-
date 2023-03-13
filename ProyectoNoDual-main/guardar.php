c16e46db6070a215da51b9b0d209965ba6d9dfea
irenecg01 <irene.s332806@cesurformacion.com>
2023-03-13 22:16:30 +0100

    chip en tabla

--- ProyectoNoDual-main/guardar.php
+++ ProyectoNoDual-main/guardar.php
@@ -1,13 +1,14 @@
 <?php
 include("inicio/conexion.php");
 
-if (isset($_POST['nombre']) && isset($_POST['tipo']) && isset($_POST['raza']) && isset($_POST['genero']) && isset($_POST['estado']) && isset($_FILES["imagen"]) && isset($_POST['descripcion'])) {
+if (isset($_POST['nombre']) && isset($_POST['tipo']) && isset($_POST['raza']) && isset($_POST['genero']) && isset($_POST['estado'])&& isset($_POST['chip'])&& isset($_FILES["imagen"]) && isset($_POST['descripcion'])) {
 
     $nombre_mascota = $_POST['nombre'];
     $tipo = $_POST['tipo'];
     $raza = $_POST['raza'];
     $genero = $_POST['genero'];
     $estado = $_POST['estado'];
+    $chip = $_POST['chip'];
     $nombre_archivo = $_FILES['imagen']['name'];
     $ruta_archivo = "img/uploads/" . $nombre_archivo;
     move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta_archivo);
@@ -16,9 +17,9 @@ if (isset($_POST['nombre']) && isset($_POST['tipo']) && isset($_POST['raza']) &&
     try {
         if ($conn) {
 
-            $query = "INSERT INTO animales (nombre, tipo, raza, genero, estado, imagen, descripcion) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $query = "INSERT INTO animales (nombre, tipo, raza, genero, estado,chip, imagen, descripcion) VALUES (?, ?, ?, ?, ? , ?, ?, ?)";
             $stmt = $conn->prepare($query);
-            $stmt->bind_param('sssssss', $nombre_mascota, $tipo, $raza, $genero, $estado, $ruta_archivo, $descripcion);
+            $stmt->bind_param('ssssssss', $nombre_mascota, $tipo, $raza, $genero, $estado,$chip, $ruta_archivo, $descripcion);
             $stmt->execute();
 
 
